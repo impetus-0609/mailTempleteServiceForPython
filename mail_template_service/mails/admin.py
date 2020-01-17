0e5c8e3310958bc2d5c1d0964a68d51c2a044311
Daiki Tezuka <ttzzkk0samu@gmail.com>
2020-01-17 20:43:52 +0900

    モデルの追加, Django管理画面に表示させる実装の追加

--- mail_template_service/mails/admin.py
+++ mail_template_service/mails/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import (Employee, Unit, Team, Section, MailTemplete)
 
-# Register your models here.
+admin.site.register(Employee)
+admin.site.register(Unit)
+admin.site.register(Team)
+admin.site.register(Section)
+admin.site.register(MailTemplete)
--- mail_template_service/mails/models.py
+++ mail_template_service/mails/models.py
@@ -25,3 +25,78 @@ class Employee(models.Model):
 
     def __str__(self):
         return '{}:{}'.format(self.id, self.name)
+
+
+class Unit(models.Model):
+    """
+    ユニット
+    """
+    CHOICE_UNIT = (
+        ('1', 'ユニット1'),
+        ('2', 'ユニット2'),
+    )
+    name = models.CharField(
+        verbose_name='ユニット名',
+        max_length=5,
+        choices=CHOICE_UNIT,
+        unique=True,
+    )
+
+
+class Team(models.Model):
+    """
+    チーム
+    """
+    name = models.CharField(
+        verbose_name='チーム名',
+        max_length=128,
+        unique=True,
+    )
+
+    unit = models.ForeignKey(
+        Unit,
+        on_delete=models.CASCADE,
+    )
+
+
+class Section(models.Model):
+    """
+    セクション
+    """
+    name = models.CharField(
+        verbose_name='セクション名',
+        max_length=128,
+        unique=True,
+    )
+
+    unit = models.ForeignKey(
+        Unit,
+        on_delete=models.CASCADE,
+    )
+
+
+class MailTemplete(models.Model):
+    """
+    メールテンプレート
+    元となる本文情報を保持.
+    """
+
+    type = models.CharField(
+        verbose_name='メール種別',
+        max_length=128,
+        unique=True,
+    )
+
+    text = models.TextField(
+        verbose_name='本文',
+        null=True,
+        blank=True,
+        max_length=1000,
+    )
+
+    # TODO 選択時にフィルインする種別とかの設定
+
+
+
+
+
